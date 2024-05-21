419e48edff2ffa61238c16eab278e4fc5de0c1e1
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-05-20 22:56:29 -0400

    NavBar done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
   return (
     <div>
       <NavBar></NavBar>
+      <div className="mx-36 flex mt-28 flex flex-col">
+        <h1 className="text-4xl">Antonio Carlos de Abreu Junior</h1>
+        <span className=" mt-8">dasdsa</span>
+      </div>
     </div>
   );
 }
--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,6 +1,16 @@
+import { ModeToggle } from "./theme";
+
 export default function NavBar() {
-    return (
-        <div>
-            <a>teste</a>
-        </div>
-    )}
+  return (
+    <div className="flex min-w-max px-1 mx-36 items-center justify-between h-16">
+      <div className="gap-4 flex flex-row text-xl">
+        <button className="hover:text-[#76ABAE]">Inicio</button>
+        <button className="hover:text-[#76ABAE]">Trabalhos</button>
+        <button className="hover:text-[#76ABAE]">Sobre</button>
+      </div>
+      <div>
+        <ModeToggle></ModeToggle>
+      </div>
+    </div>
+  );
+}
