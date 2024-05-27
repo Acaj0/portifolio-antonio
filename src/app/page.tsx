139f0372733d29d4a7ed2602c065c719d9ac919d
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-05-27 12:57:23 -0400

    Links and new section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import NavBar from "@/components/ui/NavBar";
 import Image from "next/image";
+import { Separator } from "@/components/ui/separator"
 
 export default function Home() {
   return (
@@ -15,13 +16,14 @@ export default function Home() {
               aplicações em Next.js
             </span>
             <div className="flex flex-row mt-10 gap-10 text-center items-center md:justify-start justify-center">
-              <a className="cursor-pointer h-9 outline px-4 text-center flex items-center hover:bg-black hover:text-white dark:hover:bg-white transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-300 dark:hover:text-black rounded text-xl">
+              <a href="https://1drv.ms/b/c/c8603f43faf0ff7c/EX9MPf5CoDRMl80lpkoVGiQBFJNgXSyg_Qho-6gnT-5ChQ?e=jpzpuy" target="_blank" className="cursor-pointer h-9 outline px-4 text-center flex items-center hover:bg-black hover:text-white dark:hover:bg-white transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-300 dark:hover:text-black rounded text-xl">
                 Currículo
               </a>
               <div className="w-10 flex items-center gap-0">
                 <a
                   className="transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100"
                   href="https://github.com/Acaj0"
+                  target="_blank"
                 >
                   <Image
                     className="dark:hidden"
@@ -34,6 +36,7 @@ export default function Home() {
                 <a
                   className="transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100"
                   href="https://github.com/Acaj0"
+                  target="_blank"
                 >
                   <Image
                     className="hidden dark:flex"
@@ -48,6 +51,7 @@ export default function Home() {
                 <a
                   className="transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100"
                   href="https://www.linkedin.com/in/acaj0/"
+                  target="_blank"
                 >
                   <Image
                     className="dark:hidden"
@@ -60,6 +64,7 @@ export default function Home() {
                 <a
                   className="transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-100"
                   href="https://www.linkedin.com/in/acaj0/"
+                  target="_blank"
                 >
                   <Image
                     className="hidden dark:flex"
@@ -81,6 +86,14 @@ export default function Home() {
               width={250}
               height={100}
             ></Image>
+          </div>
+          
+        </div>
+        <Separator className="mt-36" />
+        <div className="text-4xl text-center items-center justify-center flex flex-col mt-10">
+          <h2>Favoritos</h2>
+          <div className="bg-white/10 min-w-full h-44 rounded-full items-center justify-center flex mt-10">
+
           </div>
         </div>
       </div>
