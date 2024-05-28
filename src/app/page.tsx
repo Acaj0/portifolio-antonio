eda860e5db21200a23f763b4e11c973920f71dc7
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-05-28 11:36:29 -0400

    Favorites setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import NavBar from "@/components/ui/NavBar";
 import Image from "next/image";
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/separator";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function Home() {
   return (
@@ -9,14 +16,20 @@ export default function Home() {
       <div className="lg:mx-36 mx-2 flex mt-20 flex-col">
         <div className=" flex lg:flex-row flex-col min-w-fill">
           <div className=" flex flex-col">
-            <h1 className="text-5xl md:text-left text-center">Antonio Carlos de Abreu Junior</h1>
+            <h1 className="text-5xl md:text-left text-center">
+              Antonio Carlos de Abreu Junior
+            </h1>
             <span className=" mt-4 text-wrap text-center md:text-left text-2xl lg:min-w-fit">
               Eu sou estudante de Sistema de informação, sou apaixonado por
               musica e tecnologia, atualmente estou aprendendo e desenvolvendo
               aplicações em Next.js
             </span>
             <div className="flex flex-row mt-10 gap-10 text-center items-center md:justify-start justify-center">
-              <a href="https://1drv.ms/b/c/c8603f43faf0ff7c/EX9MPf5CoDRMl80lpkoVGiQBFJNgXSyg_Qho-6gnT-5ChQ?e=jpzpuy" target="_blank" className="cursor-pointer h-9 outline px-4 text-center flex items-center hover:bg-black hover:text-white dark:hover:bg-white transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-300 dark:hover:text-black rounded text-xl">
+              <a
+                href="https://1drv.ms/b/c/c8603f43faf0ff7c/EX9MPf5CoDRMl80lpkoVGiQBFJNgXSyg_Qho-6gnT-5ChQ?e=jpzpuy"
+                target="_blank"
+                className="cursor-pointer h-9 outline px-4 text-center flex items-center hover:bg-black hover:text-white dark:hover:bg-white transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-300 dark:hover:text-black rounded text-xl"
+              >
                 Currículo
               </a>
               <div className="w-10 flex items-center gap-0">
@@ -87,13 +100,49 @@ export default function Home() {
               height={100}
             ></Image>
           </div>
-          
         </div>
         <Separator className="mt-36" />
         <div className="text-4xl text-center items-center justify-center flex flex-col mt-10">
           <h2>Favoritos</h2>
-          <div className="bg-white/10 min-w-full h-44 rounded-full items-center justify-center flex mt-10">
-
+          <div className="min-w-full px-96 h-96 rounded-full items-center justify-center flex mt-10">
+            <Carousel className="min-w-full">
+              <CarouselContent className="gap-5 p-10">
+                <CarouselItem className="">
+                  <div className="border bg-black/10 rounded-lg min-w-full min-h-96 gap-2 flex flex-col 2xl:flex-row items-center justify-center">
+                    <div className="relative text-center">
+                      <Image
+                        className="blur"
+                        alt="foto"
+                        src={"/digito.jpg"}
+                        width={600}
+                        height={500}
+                      ></Image>
+                      <div className="min-w-full h-full absolute top-0 left-0 text-center justify-center flex items-center bg-black/15 rounded ">
+                        <a
+                          className="transition hover:underline"
+                          href="https://www.bandadigitozero.com.br/"
+                        >
+                          Digito Zero
+                        </a>
+                      </div>
+                    </div>
+                    <div className="max-w-96 items-center justify-center flex flex-col p-4">
+                      <h2 className="text-lg text-start text-wrap">
+                        O site da banda Digito Zero oferece aos fãs e visitantes
+                        uma plataforma envolvente e informativa, repleta de
+                        recursos exclusivos para explorar o universo musical e
+                        artístico da banda.
+                      </h2>
+                      <a className="mt-4 text-lg text-left">Saiba Mais...</a>
+                    </div>
+                  </div>
+                </CarouselItem>
+                <CarouselItem className="">...</CarouselItem>
+                <CarouselItem className="">...</CarouselItem>
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+            </Carousel>
           </div>
         </div>
       </div>
