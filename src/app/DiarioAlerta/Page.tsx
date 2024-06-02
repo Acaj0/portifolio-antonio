e41f3fa33c13f9951d089f8c99d9f946e48ac18b
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-06-02 14:23:42 -0400

    Added new pages

---
+++ src/app/DiarioAlerta/Page.tsx
@@ -0,0 +1,12 @@
+import NavBar from "@/components/ui/NavBar";
+
+export default function DiarioAlerta() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <div className="flex items-center justify-center mt-10">
+        <h1 className="text-2xl">Diario Alerta</h1>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/DigitoZero/Page.tsx
@@ -0,0 +1,12 @@
+import NavBar from "@/components/ui/NavBar";
+
+export default function DigitoZero() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <div className="flex items-center justify-center mt-10">
+        <h1 className="text-2xl">Digito Zero</h1>
+      </div>
+    </div>
+  );
+}
