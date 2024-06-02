f737701fac31090906c72ca6ec2290bd97bc84f5
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-06-02 14:57:30 -0400

    grid mobile responsiviness

--- src/app/Trabalhos/page.tsx
+++ src/app/Trabalhos/page.tsx
@@ -7,7 +7,7 @@ export default function Trabalhos() {
       <div className="flex items-center justify-center mt-10">
         <h1 className="text-2xl">Trabalhos</h1>
       </div>
-      <div className="min-w-max px-1 lg:mx-36 min-h-[550px] grid grid-rows-2 grid-flow-col gap-4 items center mb-10 mt-10">
+      <div className="min-w-max px-1 lg:mx-36 min-h-[550px] grid md:grid-rows-2 grid-rows-4 grid-flow-col gap-4 items center mb-10 mt-10">
       <div className="border-2 rounded-lg items-center flex justify-center">1</div>
       <div className="border-2 rounded-lg items-center flex justify-center">2</div>
       <div className="border-2 rounded-lg items-center flex justify-center">3</div>
