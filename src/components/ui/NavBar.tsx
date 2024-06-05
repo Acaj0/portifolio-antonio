f4d336174b59ffaa723b231a061425e68ff76f29
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-06-04 23:47:24 -0400

    Removed about

--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -28,43 +28,6 @@ export default function NavBar() {
         >
           Trabalhos
         </a>
-        <Drawer>
-          <DrawerTrigger className="hover:text-gray-500 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-50">
-            Sobre
-          </DrawerTrigger>
-          <DrawerContent>
-            <DrawerHeader>
-              <DrawerTitle className="text-center">Bem Vindo!</DrawerTitle>
-              <DrawerDescription className="text-center md:mx-[700px] mt-2">
-                Olá! Meu nome é Antonio Carlos de Abreu Junior e sou um
-                apaixonado desenvolvedor de sites, com uma missão clara:
-                transformar ideias em experiências digitais excepcionais. Com
-                experiência na criação de sites personalizados e responsivos, já
-                tive a oportunidade de colaborar com empresas de diversos
-                setores e tamanhos, sempre buscando superar as expectativas dos
-                meus clientes. Quando descobri a alegria de criar e inovar no
-                ambiente digital. Desde então, venho aprimorando minhas
-                habilidades em várias tecnologias e plataformas, incluindo HTML,
-                CSS, JavaScript, WordPress, e-commerce, e muito mais. Cada
-                projeto é uma nova aventura, onde busco soluções criativas e
-                eficientes para os desafios apresentados. Filosofia de Trabalho
-                Acredito que um bom site é aquele que combina estética,
-                funcionalidade e uma experiência de usuário intuitiva. Meu
-                processo de trabalho é colaborativo e transparente, garantindo
-                que cada cliente esteja envolvido em todas as etapas do projeto.
-                Desde a concepção inicial até o lançamento e manutenção, estou
-                comprometido em fornecer um serviço de alta qualidade que atenda
-                às necessidades específicas de cada cliente.
-              </DrawerDescription>
-            </DrawerHeader>
-            <DrawerFooter className="text-center">
-              Antonio Carlos de Abreu Junior.
-              <DrawerClose>
-                <Button variant="outline">Fechar</Button>
-              </DrawerClose>
-            </DrawerFooter>
-          </DrawerContent>
-        </Drawer>
       </div>
       <div>
         <ModeToggle></ModeToggle>
